0181dd2338e67bb833e861ec8e3167f528e2dfa7
Diwakar <diwakar.singh313@gmail.com>
2020-02-27 10:32:18 +0530

    Imlementing Auth

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Email"
+        },
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
+    }
+  };
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
+    const form = formElementsArray.map(formElement => {
+      <Input key={formElement.id} />;
+    });
+    return (
+      <div>
+        <form></form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
