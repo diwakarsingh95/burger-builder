755aded111052bf93496c0b5f1035ba59ad4a369
Diwakar <diwakar.singh313@gmail.com>
2020-07-31 01:52:11 +0530

    Updates

---
+++ precache-manifest.cc4129c007912e8c7e6006969f8e5e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92f599456032d3bb4dbdec30087aa1d",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "b972cc1480eccdebbcec",
+    "url": "/burger-builder/static/css/main.c42628df.chunk.css"
+  },
+  {
+    "revision": "2ce9a6ca60538c6e28aa",
+    "url": "/burger-builder/static/js/2.90925299.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger-builder/static/js/2.90925299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b972cc1480eccdebbcec",
+    "url": "/burger-builder/static/js/main.cf0ff8f4.chunk.js"
+  },
+  {
+    "revision": "d7a6c537ef5cb54333c5",
+    "url": "/burger-builder/static/js/runtime-main.f718073a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
